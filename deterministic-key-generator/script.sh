0d0c427beb9ae94e9778d3afba3ec77ee99dde9a
Max <mmaerov@gmail.com>
2019-10-11 01:41:09 +0300

    work

--- deterministic-key-generator/script.sh
+++ deterministic-key-generator/script.sh
@@ -4,10 +4,9 @@ sudo apt-get install -y software-properties-common python g++ make
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get update
 sudo apt-get install -y nodejs npm
-#su vagrant
-mkdir repos
-cd repos
-sudo -u vagrant git clone https://github.com/maxmayorov1977/deterministic-key-generator.git deterministic-key-generator
-cd repos/deterministic-key-generator
-#npm init
+mkdir /home/vagrant/repos
+cd /home/vagrant/repos
+git clone https://github.com/maxmayorov1977/deterministic-key-generator.git
+cd /home/vagrant/repos/deterministic-key-generator
 npm i
+#node server.js
